60e3b94f5b27e5c9cf24b998ee8603a17b744a71
Johan Bouduin <johan@der-e-coach.de>
2022-04-20 18:54:49 +0200

    2060 Site: styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,6 +35,7 @@ import { HttpClientModule } from '@angular/common/http';
     LayoutModule,
     FormsModule,
     ReactiveFormsModule,
+    MatCardModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
